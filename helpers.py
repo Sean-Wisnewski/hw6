fd00fef634ce7ed31457541a0668a038f7dfc878
Sean Wisnewski <sean.wisnewski@gmail.com>
2021-10-31 12:49:55 -0400

    copy hw5 to start

---
+++ helpers.py
@@ -0,0 +1,291 @@
+import numpy as np
+import math
+
+##################
+# Helper Classes
+##################
+class Point:
+    def __init__(self, odom):
+        self.x = odom.pose.pose.position.x
+        self.y = odom.pose.pose.position.y
+        self.w = odom.pose.pose.orientation.w
+        self.z = odom.pose.pose.orientation.z
+        self.theta = self.current_angle()
+
+    def current_angle(self):
+        return 2*math.atan2(self.z, self.w)
+
+    def __eq__(self, other):
+        return self.x == other.x and self.y == other.y
+    
+    def __repr__(self):
+        return f"({self.x},{self.y},{self.theta})"
+
+class SimplePoint:
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
+
+    def __eq__(self, other):
+        return self.x == other.x and self.y == other.y
+    
+    def __repr__(self):
+        return f"({self.x},{self.y})"
+
+
+
+##################
+# Bresenham's 
+##################
+def plotLineLow(pt0, pt1, points, num_cells_beyond=5):
+    dx = pt1.x - pt0.x
+    dy = pt1.y - pt0.y
+    yi = 1
+    if dy < 0:
+        yi = -1
+        dy = -dy
+    D = (2 * dy) - dx
+    y = pt0.y
+
+    for x in range(pt0.x, pt1.x + num_cells_beyond):
+        points.append(SimplePoint(x, y))
+        if D > 0:
+            y = y + yi
+            D = D + (2 * (dy - dx))
+        else:
+            D = D + 2 * dy
+    return points
+
+def plotLineHigh(pt0, pt1, points, num_cells_beyond=5):
+    dx = pt1.x - pt0.x
+    dy = pt1.y - pt0.y
+    xi = 1
+    if dx < 0:
+        xi = -1
+        dx = -dx
+    D = (2 * dx) - dy
+    x = pt0.x
+
+    for y in range(pt0.y, pt1.y + num_cells_beyond):
+        points.append(SimplePoint(x, y))
+        if D > 0:
+            x = x + xi
+            D = D + (2 * (dx - dy))
+        else:
+            D = D + 2 * dx
+    return points
+
+def plotLine(pt0, pt1, num_cells_beyond=5):
+    points = []
+    if abs(pt1.y - pt0.y) < abs(pt1.x - pt0.x):
+        if pt0.x > pt1.x:
+            points = plotLineLow(pt1, pt0, points, num_cells_beyond)
+        else:
+            points = plotLineLow(pt0, pt1, points, num_cells_beyond)
+    else:
+        if pt0.y > pt1.y:
+            points = plotLineHigh(pt1, pt0, points, num_cells_beyond)
+        else:
+            points = plotLineHigh(pt0, pt1, points, num_cells_beyond)
+    return points
+
+
+##################
+# Misc helpers
+##################
+def l2_dist(pt0, pt1):
+    return math.sqrt((pt1.x - pt0.x)**2 + (pt1.y - pt0.y)**2)
+
+def compute_interior_angle(pt0, pt1, pt2):
+    """
+    Computes the interior angle of a vertex given 3 points.
+    NOTE: Assumes that *pt0* is the vertex you would like to find the interior angle of
+    :param pt0:
+    :param pt1:
+    :param pt2:
+    :return:
+    """
+    angle = math.atan2(pt1.y - pt0.y, pt1.x-pt0.x) - math.atan2(pt2.y - pt0.y, pt2.x - pt0.x)
+    if angle < 0:
+        angle += 2*math.pi
+    elif angle > 2*math.pi:
+        angle -= 2*math.pi
+    return angle
+
+def compute_obstacle_point(robot_pt, d, theta, alpha):
+    """
+    Computed in real world, not rectified points - points should still be in the real world
+    coordinate system at this point, not the map world
+    """
+    # check for math.inf here, change it to be more reasonable
+    if d == math.inf:
+        d = 5 
+        print(f"changing a math.inf to {d}")
+    obs_x = robot_pt.x + d * math.cos(theta + alpha)
+    obs_y = robot_pt.y + d * math.sin(theta + alpha)
+    return SimplePoint(obs_x, obs_y)
+
+def compute_alpha_from_idx(scan, idx):
+    assert idx <= len(scan.ranges)
+    return scan.angle_min + idx * scan.angle_increment
+
+def rectify_pos_to_map_cell(pt):
+    # this will need to take an odom (x,y) pt to a map (x,y)
+    # odom will be (float, float) while map will be (int, int)
+    # the returned pt will be used in BH to get the new odds for a bunch of cells along the ray
+    x = round(pt.x, 1)
+    y = round(pt.y, 1)
+    return SimplePoint(int(x * 10), int(y * 10))
+
+def get_d_line_from_points(points, start_pt):
+    d_lines = [l2_dist(pt, start_pt) for pt in points]
+    return d_lines
+
+def convert_pt_list_to_np_idx(pts, ms):
+    xs = [pt.x+ms//2 for pt in pts]
+    ys = [pt.y+ms//2 for pt in pts]
+    return(xs, ys)
+
+def get_points_at_sonar_end(robot_map_pt, d, w, theta, alpha):
+    xdl = robot_map_pt.x + d*math.cos(theta+alpha+w/2)
+    ydl = robot_map_pt.y + d*math.sin(theta+alpha+w/2)
+    xdr = robot_map_pt.x + d*math.cos(theta+alpha-w/2)
+    ydr = robot_map_pt.y + d*math.sin(theta+alpha-w/2)
+    point_left = SimplePoint(xdl, ydl)
+    point_right = SimplePoint(xdr, ydr)
+    return point_left, point_right
+
+def get_cell_map(robot_map_pt, point_left, point_right, eps=5):
+    min_x = min(robot_map_pt.x, point_left.x, point_right.x)
+    max_x = max(robot_map_pt.x, point_left.x, point_right.x)
+    min_y = min(robot_map_pt.y, point_left.y, point_right.y)
+    max_y = max(robot_map_pt.y, point_left.y, point_right.y)
+    grid = np.mgrid[min_x-eps:max_x+eps, min_y-eps:max_y+eps].T.reshape(-1, 2)
+    return grid
+
+def change_pt_to_zero_idx(x, y, mapsize):
+    return x+mapsize//2, y+mapsize//2
+
+##################
+# Laser Update Fn 
+##################
+
+# TODO fix this to be not be some random magic number
+# realistically the max distance an object is sensed at, will probably be math.inf with the robot
+# corresponds to about 10 meters in the map, maybe make it smaller?
+MAX_D = 100 
+# WOoO random constants for proability! (technically odds)
+# will likely need to update these to make map change appropriately
+#LOW_PROB = 0.25
+#HIGH_PROB = 1.5
+#MED_HIGH_PROB = 1.2
+#MED_PROB = 0.5
+#NO_UPDATE = 1
+LOW_PROB = 0.9
+HIGH_PROB = 1.1
+MED_HIGH_PROB = 1.05
+NO_UPDATE = 1
+
+# TODO: tweak this to work with the map which looks like the scale will be in the 100's, not the 1's
+def laser_odds(d_line, d, eps=30):
+    # handle infinity
+    if d_line == math.inf:
+        d_line = MAX_D
+    # i.e. obstacle detected and we can/should update the map
+    if d < MAX_D:
+        if d_line < d-eps:
+            return LOW_PROB
+        elif d-eps <= d_line <= d+eps:
+            return HIGH_PROB
+        elif d+eps <= d_line <= d+2*eps:
+            return MED_HIGH_PROB
+        else:
+            return NO_UPDATE
+    # no obstacle detected, only update pts close to the the robot
+    else:
+        # Max dist will probably need to drop to something sane like 12m for the actual implementation
+        if d_line < d:
+            return LOW_PROB
+        else:
+            return NO_UPDATE
+
+
+##################
+# Sonar Update Fn 
+##################
+# TODO make these class variables
+MAX_DIST = 48
+NO = 1.0
+LOWEST = 0.85
+LOW = 0.9
+MED = 1.05
+HIGH = 1.1
+
+
+# TODO check that this works in all quadrants, no matter which direction the robot is facing
+# This works for at least quadrant one, need to check other quadrants
+def sonar_odds(cell_pt, rb_pt, pt_left, d, w=math.radians(30.2), w2=math.radians(5), eps=5):
+    """
+    Computes the prob/odds for a sonar pt
+    :param cell_pt:
+    :param rb_pt:
+    :param pt_left:
+    :param d: dist to the obstacle
+    :param w: width of the sonar cone
+    :param w2: indirectly, width of an "inner" sonar cone. Specified as how far in to go from the outer points
+    :param eps:
+    :return:
+    """
+    angle = compute_interior_angle(rb_pt, pt_left, cell_pt)
+    delta = l2_dist(cell_pt, rb_pt)
+    # left is slightly below zero, right slightly above 30 to account for floating pt precision nonsense
+    leftmost_angle = math.radians(-0.2)
+    inner_left_angle = leftmost_angle + w2
+    rightmost_angle = w
+    inner_right_angle = rightmost_angle - w2
+
+    # obstacle detected
+    if d != MAX_DIST:
+        # cell in the "low" zone
+        if delta < d - eps:
+            # print("in low zone")
+            if angle < leftmost_angle:
+                return NO
+            elif leftmost_angle <= angle < inner_left_angle:
+                return LOW
+            elif inner_left_angle <= angle < inner_right_angle:
+                return LOWEST
+            elif inner_right_angle <= angle <= rightmost_angle:
+                return LOW
+            else: # i.e. angle >= rightmost_angle
+                return NO
+        # cell in the "high" zone
+        elif d - eps <= delta <= d + eps:
+            # print("In high zone")
+            if angle < leftmost_angle:
+                    return NO
+            elif leftmost_angle <= angle < inner_left_angle:
+                return MED
+            elif inner_left_angle <= angle < inner_right_angle:
+                return HIGH
+            elif inner_right_angle <= angle <= rightmost_angle:
+                return MED
+            else: # i.e. angle >= rightmost_angle
+                return NO
+        # cell beyond the high zone - no update
+        else:
+            # print("beyond high zone")
+            return NO
+    # no obstacle detected, update map with low probs
+    else:
+        if angle < leftmost_angle:
+            return NO
+        elif leftmost_angle <= angle < inner_left_angle:
+            return LOW
+        elif inner_left_angle <= angle < inner_right_angle:
+            return LOWEST
+        elif inner_right_angle <= angle < rightmost_angle:
+            return LOW
+        else:
+            return NO
+
---
+++ mapper.py
@@ -0,0 +1,290 @@
+#!/usr/bin/python
+'''
+  Some Tkinter/PIL code to pop up a window with a gray-scale
+  pixel-editable image, for mapping purposes.  Does not run
+  until you fill in a few things.
+
+  Does not do any mapping.
+
+  Z. Butler, 3/2016, updated 3/2018 and 3/2020
+'''
+
+import tkinter as tk
+from PIL import Image, ImageTk
+import random, sys
+import rospy
+from sensor_msgs.msg import LaserScan
+from p2os_msgs.msg import SonarArray
+from nav_msgs.msg import Odometry
+import time
+import numpy as np
+from helpers import *
+
+# a reasonable size? depends on the scale of the map and the
+# size of the environment, of course:
+MAPSIZE = 500 
+
+class Mapper(tk.Frame):    
+
+    def __init__(self, *args, **kwargs):
+        tk.Frame.__init__(self, *args, **kwargs)
+        self.master.title("I'm the map!")
+        self.master.minsize(width=MAPSIZE,height=MAPSIZE)
+
+        # makes a grey-scale image filled with 50% grey pixels
+        # you can change the image type if you want color, check
+        # the PIL (actually, Pillow) documentation
+        self.themap = Image.new("L",(MAPSIZE,MAPSIZE),128)
+        self.mapimage = ImageTk.PhotoImage(self.themap)
+
+        # this gives us directly memory access to the image pixels:
+        self.mappix = self.themap.load()
+        # keeping the odds separately saves one step per cell update:
+        #self.oddsvals = [[1.0 for _ in range(MAPSIZE)] for _ in range(MAPSIZE)]
+        self.oddsvals = np.ones((MAPSIZE, MAPSIZE))
+
+        self.canvas = tk.Canvas(self,width=MAPSIZE, height=MAPSIZE)
+
+        self.map_on_canvas = self.canvas.create_image(MAPSIZE/2, MAPSIZE/2, image = self.mapimage)
+        self.canvas.pack()
+        self.pack()
+
+        ##############
+        # My stuff
+        ##############
+
+        ##############
+        # queues and flags
+        ##############
+        self.sonar_q = []
+        self.laser_q = []
+        self.odom_q  = []
+        self.use_sonar = False 
+        self.use_laser = False 
+        # Counter to keep track of number of updates
+        # reset to zero when LARGE_UPDATES hit, indicating to clear old values from queues
+        self.num_updates = 0
+        # alphas for the SONAR
+        self.alphas = [math.radians(90), math.radians(50), math.radians(30), math.radians(10), math.radians(-10), math.radians(-30), math.radians(-50), math.radians(-90)]
+        self.w = math.radians(30)
+
+        ##############
+        # NAMED CONSTANTS
+        ##############
+        self.OLD_TIME = 5.0
+        # clear stuff after 50 updates, or roughly 5 seconds
+        self.LARGE_UPDATES = 50
+
+
+    def update_image(self):
+        self.mapimage = ImageTk.PhotoImage(self.themap)       
+        self.canvas.create_image(MAPSIZE/2, MAPSIZE/2, image = self.mapimage)
+
+    def odds_to_pixel_value(self, value):
+        # This seems backwards for some reason rn
+        # 0 = black, 255 = white
+        if value < 1:
+            return math.floor(255 - 255*value)
+        elif 1 < value:
+            val = min(1.999, value)
+            return 255-math.floor(255*val)
+        else:
+            return 128
+
+    def sonar_update(self, smsg):
+        self.sonar_q.append(smsg)
+
+    def odom_update(self, omsg):
+        self.odom_q.append(omsg)
+        self.map_update()
+
+    def laser_update(self,lmsg):
+        self.laser_q.append(lmsg)
+
+    # here I am putting the map update in the laser callback
+    # of course you will need a sonar callback and odometry as well
+    # actually, a completely separate function on a timer might be
+    # a good idea, either way be careful if it takes longer to run
+    # than the time between calls...
+
+    def laser_map_update(self, odom, scan):
+        robot_pt = Point(odom)
+        robot_map_pt = rectify_pos_to_map_cell(robot_pt)
+        for idx, dist in enumerate(scan.ranges):
+            alpha = compute_alpha_from_idx(scan, idx)
+            # TODO take math.inf into account probably
+            # TODO probably take it into account here, maybe just throw math.inf measures away?
+            obs_pt = compute_obstacle_point(robot_pt, dist, robot_pt.theta, alpha)
+            obs_map_pt = rectify_pos_to_map_cell(obs_pt)
+            # go .3m before/after the object
+            map_pts = plotLine(robot_map_pt, obs_map_pt, 20)
+            d_lines = get_d_line_from_points(map_pts, robot_map_pt)
+            d_map_obs = l2_dist(robot_map_pt, obs_map_pt)
+            # TODO call the laser odds fn
+            lsr_odds = [laser_odds(d_line, d_map_obs, eps=3) for d_line in d_lines]
+            # this now converts to the map coordinates to be at the correct index to not go out of bounds
+            # tuple of two lists: ([x_coords], [y_coords])
+            map_pts_idx = convert_pt_list_to_np_idx(map_pts, MAPSIZE)
+            self.oddsvals[map_pts_idx] *= lsr_odds
+
+    def sonar_map_update(self, odom, ranges):
+        #print("in sonar")
+        robot_pt = Point(odom)
+        robot_map_pt = rectify_pos_to_map_cell(robot_pt)
+        for idx, dist in enumerate(ranges.ranges):
+            alpha = self.alphas[idx]
+            # to avoid having to convert dist to map dist, find the points in the
+            # real world then rectify them to the map world
+            # also note that these points are still gonna be negative, and will need to be 
+            # changed ot the correct index before updating the map/oddsvals
+            pt_l, pt_r = get_points_at_sonar_end(robot_pt, dist, self.w, robot_pt.theta, alpha)
+            pt_map_l = rectify_pos_to_map_cell(pt_l)
+            pt_map_r = rectify_pos_to_map_cell(pt_r)
+            # at this point, work in the map system, not the real world
+            cell_map = get_cell_map(robot_map_pt, pt_map_l, pt_map_r)
+            map_xs = cell_map[:, 0]
+            map_ys = cell_map[:, 1]
+            # haha I'm an idiot and still need to convert real world dist to map dist
+            map_dist = dist*10
+            snr_odds = [sonar_odds(SimplePoint(pt[0], pt[1]), robot_map_pt, pt_map_l, map_dist) for pt in cell_map] 
+
+            # I'm just gonna apologize in advance for the numpy nonsense
+            # [[x, y, sonar_odds]] - points NOT rectified
+            loc_plus_odds = np.array([map_xs, map_ys, np.array(snr_odds)]).T
+            # get idxes to actually update, i.e. places where the odds != 1
+            just_update = np.asarray(loc_plus_odds[:, 2] != 1.0).nonzero()
+            # offset the coords to be at the correct index in the map st (0,0) is in the middle
+            offset_coords = [change_pt_to_zero_idx(pt[0], pt[1], MAPSIZE) for pt in loc_plus_odds]
+            xs = [int(pt[0]) for pt in offset_coords]
+            ys = [int(pt[1]) for pt in offset_coords]
+            offset_as_idx = (xs, ys)
+            self.oddsvals[offset_as_idx] *= snr_odds
+
+    def both_map_update(self, odom, scan, ranges):
+        # call the two updates in sequence, using values from (roughly) the same timestamp
+        self.laser_map_update(odom, scan)
+        self.sonar_map_update(odom, ranges)
+
+    ##############
+    # MAP UPDATE FUNCTION
+    ##############
+    def map_update(self):
+        """
+        Updates the map! called from the odom callback, update speed will depend on if it is using laser/sonar/both
+        """
+        #rospy.loginfo("Updating the map!")
+        # TODO update to pick the correct queues to pop from
+        # jk just grab everything b/c I'm lazy
+        # this will almost certainly bite me in the ass later but oh well
+        result = self.get_vals_from_queue(self.laser_q, self.sonar_q)
+
+        # a pair or triplet of data was successfully obtained
+        if result is not None:
+            if self.use_laser and self.use_sonar:
+                #rospy.loginfo("Using both to update")
+                self.both_map_update(result[0], result[1], result[2])
+            elif self.use_laser:
+                #rospy.loginfo("Using laser to update")
+                self.laser_map_update(result[0], result[1])
+            else:
+                #rospy.loginfo("Using sonar to update")
+                self.sonar_map_update(result[0], result[2])
+            self.num_updates += 1
+            if self.num_updates == self.LARGE_UPDATES:
+                rospy.loginfo("CLEAN QUEUES")
+                self.clean_queues([self.odom_q, self.laser_q, self.sonar_q])
+            ######
+            # Actually updating the map
+            ######
+            for x in range(0, MAPSIZE):
+                for y in range(0, MAPSIZE):
+                    val = self.odds_to_pixel_value(self.oddsvals[x,y])
+                    self.mappix[x,y] = self.odds_to_pixel_value(self.oddsvals[x,y])
+
+
+
+            
+        # this puts the image update on the GUI thread, not ROS thread!
+        # also note only one image update per scan, not per map-cell update
+        self.after(0,self.update_image)    
+
+    def get_vals_from_queue(self, queue, queue_two=None, t_eps=0.15):
+        matching_flag = False
+        result = None
+        if queue_two is None:
+            while len(self.odom_q) > 0 and len(queue) > 0 and not matching_flag:
+                odom = self.odom_q.pop()
+                val = queue.pop()
+                duration = abs(odom.header.stamp - val.header.stamp)
+                t = duration.to_sec()
+                if t <= t_eps:
+                    matching_flag = True
+                    result = (odom, val)
+                    #rospy.loginfo("got a matching pair")
+                    return result
+                else:
+                    rospy.loginfo("Throwing a pair away")
+        else:
+            while len(self.odom_q) > 0 and len(queue) > 0 and len(queue_two) > 0 and not matching_flag:
+                odom = self.odom_q.pop()
+                val = queue.pop()
+                val2 = queue_two.pop()
+                duration = abs(odom.header.stamp - val.header.stamp)
+                t = duration.to_sec()
+                duration2 = abs(odom.header.stamp - val2.header.stamp)
+                t2 = duration.to_sec()
+                duration3 = abs(val.header.stamp - val2.header.stamp)
+                t3 = duration.to_sec()
+                if t <= t_eps and t2 < t_eps and t3 <= t_eps:
+                    matching_flag = True
+                    result = (odom, val, val2)
+                    #rospy.loginfo("got a matching triplet")
+                    return result
+                else:
+                    x = 5
+                    #rospy.loginfo("Throwing a pair away")
+
+    def clean_queues(self, queues):
+        lambda_fn = lambda x: (abs(time.time() - x.header.stamp.to_sec()) <= self.OLD_TIME)
+        for queue in queues:
+            rospy.loginfo(f"len before clear: {len(queue)}")
+            queue = list(filter(lambda_fn, queue))
+            rospy.loginfo(f"len after clear: {len(queue)}")
+
+
+
+
+######
+# Main
+#####
+def main():
+
+
+    rospy.init_node("mapper")
+
+    root = tk.Tk()
+    m = Mapper(master=root,height=MAPSIZE,width=MAPSIZE)
+    if len(sys.argv) != 2:
+        print("specify which device to use")
+        sys.exit()
+    else:
+        if len(sys.argv) == 2:
+            if sys.argv[1] == "laser":
+                print("use laser")
+                m.use_laser = True
+            elif sys.argv[1] == "sonar":
+                print("use sonar")
+                m.use_sonar = True
+            # make the lazy assumption that anything else means to use both
+            else:
+                m.use_laser = True
+                m.use_sonar = True
+    rospy.Subscriber("/r1/kinect_laser/scan",LaserScan,m.laser_update)
+    rospy.Subscriber("/r1/sonar/",SonarArray,m.sonar_update)
+    rospy.Subscriber("/r1/odom",Odometry,m.odom_update)
+    
+    # the GUI gets the main thread, so all your work must be in callbacks.
+    root.mainloop()
+
+if __name__ == "__main__":
+    main()
